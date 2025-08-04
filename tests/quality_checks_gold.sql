eda866d1a6f42dce9fe8dad952729690aad66c2e
anjalir28 <mathuranjali2311@gmail.com>
2025-08-04 20:59:11 +0530

    Create quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,39 @@
+
+
+
+
+-- ================================================================
+-- Checking 'gold.dim_customers'
+-- ================================================================
+-- Check for Uniqueness of Product Key in gold.dim_customers
+-- Expectations: No Results
+SELECT
+  customer_key,
+  COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ================================================================
+-- Checking 'gold.product_key'
+-- ================================================================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectations: No Results
+SELECT
+  product_key,
+  COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ================================================================
+-- Checking 'gold.fact_sales'
+-- ================================================================
+-- Check the data model connectivity between fact and dimensions
+SELECT *
+FROM gold.facts_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL
